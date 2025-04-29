09dcff7295ec43cb4451a54d56c63e104dded71a
helosantosdesousa <ssheloly@gmail.com>
2025-04-29 12:12:52 -0300

    add logo e new buttons on app.py

--- app.py
+++ app.py
@@ -12,11 +12,39 @@ print(questions)
 window=tk.Tk()
 window.title('Quiz')
 window.geometry('400x450')
-background_color = '#ECECEC'
+
+bg_color = '#ECECEC'
 text_color = '#333333'
 bt_color = '#4CAF50'
+bt_text_color = '#FFFFFF'
 
-window.config(bg=background_color)
+window.config(bg=bg_color)
 window.option_add('*Font', 'Arial')
+
+# icon
+app_icon = PhotoImage(file='logo.png')
+app_label = tk.Label(window, image=app_icon,bg=bg_color)
+app_label.pack(pady=10)
+
+# ui componets
+questions_label = tk.Label(window, text='Question', wraplength=380, bg=bg_color, fg=text_color, font=('Arial',12,'bold'))
+questions_label.pack(pady=20)
+
+right_answer = tk.IntVar()
+op1_btn = tk.Button(window, text='', width=30, bg=bt_color, fg=bt_text_color, state=tk.DISABLED, font=('Arial', 10, 'bold'))
+op1_btn.pack(pady=10)
+
+op2_btn = tk.Button(window, text='', width=30, bg=bt_color, fg=bt_text_color, state=tk.DISABLED, font=('Arial', 10, 'bold'))
+op2_btn.pack(pady=10)
+
+op3_btn = tk.Button(window, text='', width=30, bg=bt_color, fg=bt_text_color, state=tk.DISABLED, font=('Arial', 10, 'bold'))
+op3_btn.pack(pady=10)
+
+op4_btn = tk.Button(window, text='', width=30, bg=bt_color, fg=bt_text_color, state=tk.DISABLED, font=('Arial', 10, 'bold'))
+op4_btn.pack(pady=10)
+
+
+play_again_btn = tk.Button(window, text='Play again', width=30, bg=bt_color, fg=bt_text_color, font=('Arial', 10, 'bold'))
+play_again_btn.pack(pady=10)
 
 window.mainloop()
